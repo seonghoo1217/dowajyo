3589144686e6a86e6166c5ba1d7582397951e5e3
Lee.t.c <seonghoo1217@naver.com>
2022-03-16 21:45:39 +0900

    Dowajyo Project 기초 셋팅

---
+++ src/main/java/com/dowajyo/DowajyoApplication.java
@@ -0,0 +1,13 @@
+package com.dowajyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DowajyoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DowajyoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dowajyo/DowajyoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dowajyo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DowajyoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
